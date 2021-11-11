25bfb8276f0ef25ee7a411865dc0afefe48d0635
Parth Thakur <parththakur@gmail.com>
2021-11-11 14:08:02 +0000

    Add readability.c

---
+++ week 2/readability/readability.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+
+int letterCount(string s);
+float wordCount(string s);
+int sentenceCount(string s);
+
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    
+    float W = wordCount(text);
+    float index = 0.0588 * ((letterCount(text) / W) * 100) - 0.296 * ((sentenceCount(text) / W) * 100) - 15.8;
+    int grade = round(index);
+
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
+
+
+int letterCount(string s)
+{
+    int letter_count = 0;
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (toupper(s[i]) >= 'A' && toupper(s[i]) <= 'Z')
+        {
+            letter_count += 1;
+        }
+    }
+
+    return letter_count;
+}
+
+
+float wordCount(string s)
+{
+    float word_count = 1.0;
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] == ' ')
+        {
+            word_count += 1;
+        }
+    }
+
+    return word_count;
+}
+
+
+int sentenceCount(string s)
+{
+    int sentence_count = 0;
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] == '.' || s[i] == '?' || s[i] == '!')
+        {
+            sentence_count += 1;
+        }
+    }
+
+    return sentence_count;
+}

7428d02874393e73c890a19c1fe3633ed3e52a8d
Parth Thakur <parththakur@gmail.com>
2021-12-22 23:56:29 +0000

    Check for duplicates in key

--- week 2/substitution/substitution.c
+++ week 2/substitution/substitution.c
@@ -1,6 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+
+bool check_valid(string key)
+{
+    bool letters[26] = {false};
+    for (int i = 0; i < 26; i++)
+    {
+        letters[tolower(key[i]) - 97] = true;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (!letters[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 int main(int argc, string argv[])
@@ -11,9 +31,20 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    if (strlen(argv[0])  != 26)
+    string key = argv[1];
+    if (strlen(key)  != 26)
     {
         printf("Key must contain 26 characters. \n");
         return 1;
     }
+
+    if (! check_valid(key))
+    {
+        printf("Entered key is invalid. \n");
+        return 1;
+    }
+
+    printf("%s\n", key);
+
+
 }
